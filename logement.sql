132303b7f5d57f69e91be58e4f704c4842232d35
BaoHuiling <baozii.721@gmail.com>
2020-12-14 15:49:49 +0100

    2

---
+++ logement.sql
@@ -0,0 +1,139 @@
+-- sqlite3 bibli.db
+-- .read logement.sql
+
+-------
+-- commandes de destruction des tables
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Logement;
+DROP TABLE IF EXISTS Adresse;
+DROP TABLE IF EXISTS Ville;
+DROP TABLE IF EXISTS Chambre;
+DROP TABLE IF EXISTS Coordonnees;
+DROP TABLE IF EXISTS Capteur;
+DROP TABLE IF EXISTS TypeCap;
+DROP TABLE IF EXISTS LogeCham;
+DROP TABLE IF EXISTS ChamCap;
+--DROP TABLE IF EXISTS FacLog;
+DROP TABLE IF EXISTS Mesure;
+DROP TABLE IF EXISTS Facture;
+
+-------
+-- commandes de creation des tables
+
+CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, user_name TEXT NOT NULL, pwd TEXT NOT NULL);
+
+-- Table identifiant un logement par son id, l'adresse, son numero de telephone, une adresse IP et la date d'insertion
+CREATE TABLE Logement (id INTEGER PRIMARY KEY AUTOINCREMENT, idAd INTEGER NOT NULL, Nbphone TEXT NOT NULL, Aip TEXT NOT NULL, TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (idAd) REFERENCES Adresse(id));
+-- Table identifiant l'adresse d'un logement: id unique, numero, voie et le nom du voie, code postal
+CREATE TABLE Adresse (id  INTEGER PRIMARY KEY AUTOINCREMENT, Numero INTEGER NOT NULL, Voie TEXT NOT NULL, Nom_voie TEXT NOT NULL, Code INTEGER NOT NULL, FOREIGN KEY (Code) REFERENCES Ville(Code));
+-- Table identifiant une ville: code postal, nom de la ville
+CREATE TABLE Ville (Code INTEGER PRIMARY KEY, Nom TEXT NOT NULL);
+
+-- Table identifiant une chambre: id unique, nom de la chambre, sa coordonnee dans le logement correspondant
+CREATE TABLE Chambre (id INTEGER PRIMARY KEY AUTOINCREMENT, Nom_Cham TEXT NOT NULL, idCor INTEGER NOT NULL, FOREIGN KEY (idCor) REFERENCES Coordonnees(id));
+-- Table des coordonnees des chambres: matrice 3D donc 3 champs D1,D2,D3
+CREATE TABLE Coordonnees (id INTEGER PRIMARY KEY AUTOINCREMENT,	 D1 INTEGER NOT NULL, D2 INTEGER NOT NULL, D3 INTEGER NOT NULL);
+
+-- Table identifiant un capteur: id unique, le type du capteur, reference commercial, id du chambre ou il se situe, port de communication, date d'insertion
+CREATE TABLE Capteur (id INTEGER PRIMARY KEY AUTOINCREMENT, idTyCap INTEGER NOT NULL, RfComm TEXT NOT NULL, Port INTEGER NOT NULL, TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(idTyCap) REFERENCES TypeCap(id));
+-- Table identifiant le type du capteur: id unique, le type(température,électricité etc), unite de mesure, plage de precision
+CREATE TABLE TypeCap (id INTEGER PRIMARY KEY AUTOINCREMENT, Type TEXT NOT NULL, Unite TEXT NOT NULL, Precision TEXT NOT NULL);
+
+-------
+-- insertion de données
+INSERT INTO Users (user_name, pwd) VALUES ("Huiling", "password");
+INSERT INTO Users (user_name, pwd) VALUES ("Tom", "password");
+INSERT INTO Users (user_name, pwd) VALUES ("Lily", "password");
+
+-- Ajouter un logement avec 4 pieces
+INSERT INTO Ville (Code, Nom) VALUES (94200, "Ivry Sur Seine");
+INSERT INTO Adresse (Numero, Voie, Nom_voie, Code) VALUES (89, "Avenue", "Maurice Thorez", 94200);
+INSERT INTO Logement (idAd, Nbphone, Aip) VALUES(1, "0638395725", "127.0.0.1");
+INSERT INTO Coordonnees (D1, D2, D3) VALUES (1,1,1),(1,2,1),(2,1,1),(2,2,1);
+INSERT INTO Chambre (Nom_Cham, idCor) VALUES
+	("living room", 1), 
+	("dinning room", 2), 
+	("bathroom", 3),
+	("bedroom", 4);
+
+-- Ajouter 4 type de capteur
+INSERT INTO TypeCap (Type, Unite, Precision) VALUES
+	("temperature", "°C", "0.1"),
+	("humidity", "%RH", "0.1"),
+	("light", "Lux", "1"),
+	("ultrasonic", "cm", "1");
+
+-- Ajouter 2 capteur 
+INSERT INTO Capteur (idTyCap, RfComm, Port) VALUES
+	(1, "6010344", 80),
+	(2, "6010344", 81);
+
+-------
+-- Associations
+-- Table represente quel logement possede quelle(s) chambre(s): id du logement, id du Chambre
+CREATE TABLE LogeCham (idLoge INTEGER NOT NULL, idCham INTEGER NOT NULL, FOREIGN KEY (idLoge) REFERENCES Logement(id), FOREIGN KEY (idCham) REFERENCES Chambre(id), PRIMARY KEY (idLoge,idCham));
+-- Table represente quelle Chambre possede quel(s) capteur(s): id de la chambre, id du capteur
+CREATE TABLE ChamCap (idCham INTEGER NOT NULL, idCap INTEGER NOT NULL, FOREIGN KEY (idCham) REFERENCES Chambre(id), FOREIGN KEY (idCap) REFERENCES Capteur(id), PRIMARY KEY (idCham,idCap));
+-- Table represente les factures aliment quel logement:
+--CREATE TABLE FacLog (idFac INTEGER NOT NULL, idLoge INTEGER NOT NULL, FOREIGN KEY (idFac) REFERENCES Facture(id), FOREIGN KEY (idLoge) REFERENCES Logement(id), PRIMARY KEY (idFac,idLoge));
+
+-------
+-- Actions
+-- Table decrit une mesure: id du capteur utilise, valeur de la mesure, moment de la mesure
+CREATE TABLE Mesure (id INTEGER PRIMARY KEY AUTOINCREMENT, idCap INTEGER NOT NULL, Valeur REAL NOT NULL, TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (idCap) REFERENCES Capteur(id));
+-- Table identifiant la facture d'un logement: id unique, logement associe, type de la facture, valeur consomme, montant de la facture et la date d'insertion
+CREATE TABLE Facture (id INTEGER PRIMARY KEY AUTOINCREMENT, idLoge INTEGER NOT NULL, Type TEXT NOT NULL, Valcsm REAL NOT NULL, Montant REAL NOT NULL, TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (idLoge) REFERENCES Logement(id));
+
+-------
+-- insertion de données
+
+-- Associer les 4 pieces a ce logement:
+INSERT INTO LogeCham (idLoge, idCham) VALUES 
+	(1, 1), 
+	(1, 2), 
+	(1, 3),
+	(1, 4);
+-- Associer les 2 capteurs aux differentes chambre: temperature dans le living room et humidity dans le bedroom
+INSERT INTO ChamCap (idCham, idCap) VALUES 
+	(1, 1), 
+	(4, 2);
+
+-- 2 mesure par capteur:
+INSERT INTO Mesure (idCap, Valeur) VALUES
+	(1, 28.9),
+	(1, 29.1),
+	(2, 35.6),
+	(2, 34.4);
+
+-- 4 facture:
+INSERT INTO Facture (idLoge, Type, Valcsm, Montant) VALUES
+	(1, "electricite", 		10.2, 32),
+	(1, "electricite", 		6.9, 21),
+	(1, "electricite", 		8.3, 25),
+	(1, "electricite", 		4.9, 15),
+	(1, "electricite", 		7.2, 21.9),
+	(1, "eau", 		   		18.9, 12.9),
+	(1, "eau", 		   		24.5, 15.7),
+	(1, "eau", 		   		21.0, 13.4),
+	(1, "eau", 		   		28.7, 20.9),
+	(1, "eau", 		   		11.4, 9.9),
+	(1, "chauffage",   		1,   20.5),
+	(1, "chauffage",   		1,   18.0),
+	(1, "chauffage",   		1,   24.9),
+	(1, "chauffage",   		1,   29.2),
+	(1, "chauffage",   		1,   28.9),
+	(1, "dechet",   		1,   3.5),
+	(1, "dechet",   		1,   5.7),
+	(1, "dechet",   		1,   2.4),
+	(1, "dechet",   		1,   9.3),
+	(1, "dechet",   		1,   2.3),
+	(1, "service internet", 1,   13),
+	(1, "service internet", 1,   10),
+	(1, "service internet", 1,   9),
+	(1, "service internet", 1,   8),
+	(1, "repair wall", 1,   25);
+
+
+
+
+
---
+++ serveur.py
@@ -0,0 +1,387 @@
+#curl -X GET http://localhost:8888/chart pour afficher le chart de Facture
+#curl -X GET http://localhost:8888/meteo  
+import http.server, urllib.parse, sqlite3, threading, socketserver, requests, json, random
+
+def meteo():
+	cs_url  = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=554632cfaef8c83ef4f1f5aec5fbe697'
+	r = requests.get(cs_url)
+ 	#d_text = json.loads(text)
+	return r.json()
+
+# TODO
+def hellopage():
+
+	hellopage = '\
+	<html>\n\
+	<body>\n\
+	<h1>Welcome to our platform!</h1> \n\
+		<form action="http://localhost:8888/login" method="post">\n\
+		User name: <input type="text" name="user_name"><br>\n\
+		Password: <input type="password" name="pwd"><br>\n\
+		<input type="submit" value="Login">\n\
+		</form>\n\
+	</body>\n\
+	</html>\n\
+	'
+	return hellopage
+
+def hellopage_alarm():
+
+	hellopage = '\
+	<html>\n\
+	<body>\n\
+	<p>Please enter valid user name or password</p> \n\
+		<p><a href="http://localhost:8888/"></a></p>\n\
+		<button onclick="myFunction()">Go back</button>\n\
+	<script>\n\
+		function myFunction() {\
+		  window.location.href="http://localhost:8888/";\
+		}\n\
+	</script>\n\
+	</body>\n\
+	</html>\n\
+	'
+
+	return hellopage
+
+
+def menu():
+
+	menu = "<html> \n\
+					<body>\n\
+					<h1>Welcome!</h1> \n\
+					<h3>Choose the fonction: </h3>\n\
+					<p><a href=\"http://localhost:8888/chart\">See the consumption</a></p>\n\
+					<p><a href=\"http://localhost:8888/mesure\">Check the status of the sensors / actuators</a></p>\n\
+					<p><a href=\"http://localhost:8888/save\">See the savings achieved</a></p>\n\
+					<p><a href=\"http://localhost:8888/config\">Configuration</a></p>\n\
+				</body>\n\
+				</html>\n"
+	return menu
+
+def make_chart(facture):
+	elec=[]
+	eau=[]
+	chauffage=[]
+	dechet=[]
+	autre=[]
+	fac=" "
+	for row in facture:
+		if row[2]=="electricite":
+			elec.append(row[4])
+		elif row[2]=="eau":
+			eau.append(row[4])
+		elif row[2]=="chauffage":
+			chauffage.append(row[4])
+		elif row[2]=="dechet":
+			dechet.append(row[4])
+		else:
+			autre.append(row[4])
+
+	#lenmax = max(len(elec), len(eau), len(chauffage), len(dechet), len(autre))
+	lenmax = 5;
+	for i in range(lenmax):
+		col = "[%s,%s,%s,%s,%s,%s]" %(i+1,elec[i],eau[i],chauffage[i],dechet[i],autre[i])
+		fac += col;
+		if(i<lenmax-1):
+			fac+=","
+
+	chart = "\
+	  <html>\n\
+  		<head>\n\
+    		<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\n\
+    		<script type=\"text/javascript\">\n\
+      		google.charts.load('current', {'packages':['corechart']});\n\
+	      google.charts.setOnLoadCallback(drawChart);\n\
+	      function drawChart() {\n\
+	        var data = google.visualization.arrayToDataTable([\
+	          ['number', 'Electricity', 'Water', 'Warm gas', 'Garbage','Else'],%s]);\n\
+	        var options = {\
+	          title: 'Consumption of residence in euro (EUR)',\
+	          curveType: 'function',\
+	          legend: { position: 'bottom' }\
+	        };\n\
+	        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));\n\
+	        chart.draw(data, options);\n\
+	      }\n\
+	    </script>\n\
+	  </head>\n\
+	  <body>\n\
+	    <div id=\"curve_chart\" style=\"width: 900px; height: 500px\"></div>\n\
+	  </body>\n\
+	</html>" %(fac)
+	
+	return chart
+
+def config():
+
+	config = "\
+	<html>\n\
+	<body>\n\
+	<h1>Add a new sensor</h1> \n\
+	<form action=\"index.html\" method=\"post\">\n\
+	Sensor type id: <input type=\"text\" name=\"idTyCap\"><br>\n\
+	Commercial reference: <input type=\"text\" name=\"RfComm\"><br>\n\
+	Port: <input type=\"text\" name=\"Port\"><br>\n\
+	<input type=\"submit\" value=\"submit\">\n\
+	</form>\n\
+	</body>\n\
+	</html>\n\
+	"
+	return config
+
+def mesure(self,mes):
+	
+	m = ""
+	length = len(mes)
+	i=1
+	for row in mes:
+		
+		req = "select idTyCap from Capteur where id=%s" %(row[1])
+		idTyCap = self.c.execute(req).fetchall()
+		req = "select Type from TypeCap where id=%s" %(idTyCap[0][0])
+		typet = self.c.execute(req).fetchall()
+		Type = typet[0][0]
+
+		m += "[%s,\'%s\',%s,\'%s\']" %(row[1],Type,row[2],row[3])
+		if(i<length):
+			m += ","
+		i=i+1
+
+	print (m)
+	mesure = "<html>\n\
+  <head>\n\
+    <script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\n\
+    <script type=\"text/javascript\">\n\
+      google.charts.load('current', {'packages':['table']});\n\
+      google.charts.setOnLoadCallback(drawTable);\n\
+      function drawTable() {\
+        var data = new google.visualization.DataTable();\n\
+        data.addColumn('number', 'Capture');\n\
+        data.addColumn('string', 'Type');\n\
+        data.addColumn('number', 'Value');\n\
+        data.addColumn('string', 'Time');\n\
+        data.addRows([%s]);\n\
+        var table = new google.visualization.Table(document.getElementById('table_div'));\n\
+        table.draw(data,{showRowNumber: true, width: '900px', height: '500px'});\n\
+      }\n\
+    </script>\n\
+  </head>\n\
+  <body>\n\
+    <div id=\"table_div\"></div>\"\"\n\
+  </body>\n\
+</html>\n" %(m)
+	print(mesure)
+	return mesure
+
+def save(facture):
+	elec=[]
+	eau=[]
+	chauffage=[]
+	dechet=[]
+	autre=[]
+	fac=""
+	for row in facture:
+		if row[2]=="electricite":
+			elec.append(row[4])
+		elif row[2]=="eau":
+			eau.append(row[4])
+		elif row[2]=="chauffage":
+			chauffage.append(row[4])
+		elif row[2]=="dechet":
+			dechet.append(row[4])
+		else:
+			autre.append(row[4])
+
+	#lenmax = max(len(elec), len(eau), len(chauffage), len(dechet), len(autre))
+	lenmax = 5;
+	for i in range(lenmax):
+		som = elec[i]+eau[i]+chauffage[i]+dechet[i]+autre[i]
+		col = "[%s,%s,%s]" %(i+1,som-random.randint(15,50),som) #random pour simuler la valeur original(unconnu)
+		fac += col;
+		if(i<lenmax-1):
+			fac+=","
+
+	chart ='''<html>\n\
+
+  <head>\n\
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>\n\
+    <script type="text/javascript">\n\
+      google.charts.load('current', {'packages':['corechart']});\n\
+      google.charts.setOnLoadCallback(drawChart);\n\
+      function drawChart() {\
+        var data = google.visualization.arrayToDataTable([\
+          ['time', 'original', 'eco-responsable'],\
+          %s]);\n\
+        var options = {\
+          title: 'Comparaison of orginal consumption and eco-responsable',\
+          hAxis: {title: 'time',  titleTextStyle: {color: '#333'}},\
+          vAxis: {minValue: 0}\
+        };\n\
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));\n\
+        chart.draw(data, options);\n\
+      }\n\
+    </script>\n\
+  </head>\n\
+  <body>\n\
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>\n\
+  </body>\n\
+</html>\n''' %(fac)
+	print(chart)
+	return chart
+
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+	def __init__(self, *args, **kwargs):
+		self.mysql = MySQL('logement.db')
+		super(MyHandler, self).__init__(*args, **kwargs)
+
+	def do_GET(self):
+		"""Respond to a GET request."""
+		if self.path == '/favicon.ico':
+			return 
+		if self.path == '/':    #localhost:8888/
+			self.send_response(200)
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+			html  = hellopage()
+			self.wfile.write(bytes(str(html)+'\n', 'UTF-8'))
+		else:
+			res = urllib.parse.urlparse(self.path)
+			rep = self.mysql.select(res.path)
+			if len(rep) > 0:
+				self.send_response(200)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
+				self.wfile.write(bytes(str(rep)+'\n', 'UTF-8'))
+			else:
+				self.send_response(404)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
+		
+	def do_POST(self):
+		"""Respond to a POST request."""
+		print("POST" + self.path)
+		if self.path == "/index.html":
+			q = self.rfile.read(int(self.headers['content-length'])).decode(encoding="utf-8")
+			query = urllib.parse.parse_qs(q,keep_blank_values=1,encoding='utf-8')
+			path = "/Capteur"
+			rep = self.mysql.insert(path,query)
+			self.send_response(200)
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+		elif self.path == "/login":
+			q = self.rfile.read(int(self.headers['content-length'])).decode(encoding="utf-8")
+			query = urllib.parse.parse_qs(q,keep_blank_values=1,encoding='utf-8')
+			
+			rep = self.mysql.check_login(self.path,query)
+
+			if len(rep) > 0:
+				self.send_response(200)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
+				self.wfile.write(bytes(str(rep)+'\n', 'UTF-8'))
+
+		else:
+			res = urllib.parse.urlparse(self.path)
+			path = res.path
+			query = urllib.parse.parse_qs(res.query)
+			rep = self.mysql.insert(path,query)
+			self.send_response(200)
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+
+class MySQL():
+	def __init__(self, name):
+		self.c = None
+		self.req = None
+		self.conn = sqlite3.connect(name,check_same_thread=False)
+		self.c = self.conn.cursor()
+
+	def __exit__(self, exc_type, exc_value, traceback):
+		self.conn.close()
+
+	def select(self,path):
+		elem = path.split('/')
+		if(elem[1]=="chart"):
+			print("chart part")
+			req = "select * from Facture"
+			facture = self.c.execute(req).fetchall()
+			return make_chart(facture)
+		if(elem[1]=="meteo"):
+			print("meteo request")
+			return meteo()
+		# if(elem[1]=="config"):
+		# 	print("config request")
+		# 	return config()
+		if(elem[1]=="mesure"):
+			print("mesure request")
+			req = "select * from Mesure"
+			mes = self.c.execute(req).fetchall()
+			return mesure(self,mes)
+		if(elem[1]=="save"):
+			print("save request")
+			req = "select * from Facture"
+			fac = self.c.execute(req).fetchall()
+			return save(fac)
+		elif len(elem) == 2:
+			req = "select * from %s" %(elem[1])
+		elif len(elem) == 3:
+			req = "select * from %s where id=%s" %(elem[1],elem[2])
+		elif len(elem) == 4:
+			req = "select %s from %s where id=%s" %(elem[3],elem[1],elem[2])
+		elif len(elem) == 5:
+			req = "select %s from %s where %s=%s" %(elem[4],elem[1],elem[2],elem[3])
+		else:
+			print("GET :URI format error")
+		return self.c.execute(req).fetchall()
+	
+	def insert(self,path,query):
+		attr = ', '.join(query.keys())
+		val = ', '.join('"%s"' %v[0] for v in query.values())
+		print(attr,val)
+		req = "insert into %s (%s) values (%s)" %(path.split('/')[1], attr, val)
+		print(req)
+		self.c.executescript(req)
+		self.conn.commit()
+		return 1
+
+	def check_login(self,path,query):
+		name = "".join(query['user_name'])
+		print(name)
+		pwd = "".join(query["pwd"])
+		print(pwd)
+		req = 'SELECT pwd FROM Users WHERE user_name="%s";' %(name)
+		print(req)
+		res = self.c.execute(req).fetchall()
+		res = res[0][0]
+		print(res)
+		if (len(res)>0) and (pwd == res):
+			return config()
+
+		return hellopage_alarm()
+
+
+class ThreadingHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
+	pass
+
+def serve_on_port(port):
+	server = ThreadingHTTPServer(("localhost", port), MyHandler)
+	server.serve_forever()
+
+if __name__ == '__main__':
+	# mono thread
+	# server_class = http.server.HTTPServer
+	# httpd = server_class(("localhost", 8888), MyHandler)
+	#try:
+		#httpd.serve_forever()
+	#except KeyboardInterrupt:
+		#pass
+	#httpd.server_close()
+
+	# multi-thread
+	threading.Thread(target=serve_on_port, args=[7777]).start()
+	threading.Thread(target=serve_on_port, args=[8888]).start()
+	threading.Thread(target=serve_on_port, args=[9999]).start()
+	
+	
