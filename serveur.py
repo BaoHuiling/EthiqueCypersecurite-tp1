db31e81cf1ef3dbdeea8ee4b8f0138b014594ac2
28612380 <yingshan19980105@gmail.com>
2020-12-28 12:52:34 -0800

    version3,beautiful login page

--- serveur.py
+++ serveur.py
@@ -11,18 +11,93 @@ def meteo():
 # TODO
 def hellopage():
 
-	hellopage = '\
-	<html>\n\
-	<body>\n\
-	<h1>Welcome to our platform!</h1> \n\
-		<form action="http://localhost:8888/login" method="post">\n\
-		User name: <input type="text" name="user_name"><br>\n\
-		Password: <input type="password" name="pwd"><br>\n\
-		<input type="submit" value="Login">\n\
-		</form>\n\
-	</body>\n\
-	</html>\n\
-	'
+	hellopage = hellopage = '''
+<!DOCTYPE html>  
+<html lang="en">  
+<head>  
+    <meta charset="UTF-8">  
+    <title>Login</title>  
+	<style>
+		html{   
+    width: 100%;   
+    height: 100%;   
+    overflow: hidden;   
+    font-style: sans-serif;   
+}   
+body{   
+    width: 100%;   
+    height: 100%;   
+    font-family: 'Open Sans',sans-serif;   
+    margin: 0;   
+    background-color: #4A374A;   
+}   
+#login{   
+    position: absolute;   
+    top: 50%;   
+    left:50%;   
+    margin: -150px 0 0 -150px;   
+    width: 300px;   
+    height: 300px;   
+}   
+#login h1{   
+    color: #fff;   
+    text-shadow:0 0 10px;   
+    letter-spacing: 1px;   
+    text-align: center;   
+}   
+h1{   
+    font-size: 2em;   
+    margin: 0.67em 0;   
+}   
+h2
+{
+	color:white;
+	text-align:center;
+}
+input{   
+    width: 278px;   
+    height: 18px;   
+    margin-bottom: 10px;   
+    outline: none;   
+    padding: 10px;   
+    font-size: 13px;   
+    color: #fff;   
+    text-shadow:1px 1px 1px;   
+    border-top: 1px solid #312E3D;   
+    border-left: 1px solid #312E3D;   
+    border-right: 1px solid #312E3D;   
+    border-bottom: 1px solid #56536A;   
+    border-radius: 4px;   
+    background-color: #2D2D3F;   
+}   
+.but{   
+    width: 300px;   
+    min-height: 20px;   
+    display: block;   
+    background-color: #4a77d4;   
+    border: 1px solid #3762bc;   
+    color: #fff;   
+    padding: 9px 14px;   
+    font-size: 15px;   
+    line-height: normal;   
+    border-radius: 5px;   
+    margin: 0;   
+}  
+	</style>
+</head>  
+<body>  
+    <div id="login">  
+        <h1>Login</h1>  
+		<h2>For SQL Injection Test</h2>
+        <form action="http://localhost:8888/login" method="post">  
+            <input type="text" required="required" placeholder="Username" name="user_name"></input>  
+            <input type="password" required="required" placeholder="Password" name="pwd"></input>  
+            <button class="but" type="submit">Go!</button>  
+        </form>  
+    </div>  
+</body>  
+</html> 
+	'''
 	return hellopage
 
 def hellopage_alarm():
